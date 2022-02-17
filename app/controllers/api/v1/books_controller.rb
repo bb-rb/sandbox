a52f199afe65b5b9b26b3466028d4a5d1a88e917
Roman Burban <roman.burban@barbuck.com>
2022-02-17 23:06:23 +0100

    add tests to create & delete, update test for index/get

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,13 +1,12 @@
 module Api
   module V1
     class BooksController < ApplicationController
-      
       def index
         render json: Book.all
       end
 
       def create
-        book = Book.new(title: params[:title], author: params[:author])
+        book = Book.new(book_params)
 
         if book.save
           render json: book, status: :created
@@ -22,14 +21,11 @@ module Api
         head :no_content
       end
 
-
       private
 
       def book_params
         params.require(:book).permit(:title, :author)
-        
       end
-      
     end
   end
 end
