53f9e5f9fdf1f3ac7a0351a6da7c4b40ac1beaaa
Roman Burban <roman.burban@barbuck.com>
2022-02-17 11:56:27 +0100

    Separate Api routes from regular routes in en application

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # get 'books/index' (auto-generated with "bin/rails g controller BooksController index")
-  resources :books, only: [:index, :create, :destroy]
+  namespace :api do
+    namespace :v1 do
+      resources :books, only: [:index, :create, :destroy]
+    end
+  end
 end
