6dbaaa683713842abaa23276bffeea0ca073e98a
Roman Burban <roman.burban@barbuck.com>
2022-02-17 01:08:46 +0100

    Adding controller to deleting books

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # get 'books/index' (auto-generated with "bin/rails g controller BooksController index")
-  resources :books, only: [:index, :create]
+  resources :books, only: [:index, :create, :destroy]
 end
